d942a7694d1f2bb6603583e6a45b65ea07c3af80
simonvargas <simonvargas01@gmail.com>
2021-07-14 19:24:18 -0400

    phase 5

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -37,6 +37,19 @@ router.delete(
     }
   );
   
+router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        return res.json({
+          user: user.toSafeObject()
+        });
+      } else return res.json({});
+    }
+  );
+  
   
 
 
