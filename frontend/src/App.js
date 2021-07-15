ebc445728b21c322563c1b0c0e90da98dfe17702
simonvargas <simonvargas01@gmail.com>
2021-07-15 17:12:00 -0400

    finished 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,33 @@
+import { Route, Switch } from 'react-router-dom'
+import LoginFormPage from './components/LoginFormModal/index'
+import { useDispatch } from 'react-redux'
+import { useState, useEffect } from 'react'
+import * as sessionActions from "./store/session";
+import SignupFormPage from './components/SignupForm';
+import Navigation from './components/Navigation';
+// import { LoginFormModal } from './components/LoginFormModal/index'
+
 function App() {
-  return (
-    <h1>Hello from App</h1>
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+  }, [dispatch]);
+
+ 
+
+  return isLoaded && (
+    <>
+    <Navigation isLoaded={isLoaded} />
+    {isLoaded && (
+    <Switch>
+    
+      <Route path="/signup">
+        <SignupFormPage />
+      </Route>
+    </Switch>
+    )}
+    </>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,8 @@ import { Provider }  from 'react-redux';
 import { BrowserRouter } from 'react-router-dom'
 import { restoreCSRF, csrfFetch } from './store/csrf';
 import configureStore from './store';
+import * as sessionActions from './store/session';
+import { ModalProvider } from "./context/Modal";
 
 const store = configureStore()
 
@@ -14,14 +16,17 @@ if (process.env.NODE_ENV !== 'production') {
 
   window.store = store;
   window.csrfFetch = csrfFetch;
+  window.sessionActions = sessionActions;
 }
 
 function Root() {
   return (
     <Provider store={store}>
+      <ModalProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
+    </ModalProvider>
     </Provider>
   );
 };
