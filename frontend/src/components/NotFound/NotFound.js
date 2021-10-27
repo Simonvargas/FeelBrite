0568b6caa333b255089be30e2add44843ae6958f
simonvargas <simonvargas01@gmail.com>
2021-10-26 22:45:12 -0400

    404

--- frontend/src/components/NotFound/NotFound.js
+++ frontend/src/components/NotFound/NotFound.js
@@ -1,13 +1,14 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 import styles from './NotFound.module.css'
 
 const NotFound = () => {
 
     return (
     <div className={styles.body}>
-        <h1 className={styles.h2}>404 Page not found!</h1>
-        <img className={styles.newImg} src='https://i.imgur.com/QnAPvcN.png'></img>
+        
+        <h1 className={styles.h2}> 404 Page not found!</h1>
+        <Link to='/'><img className={styles.newImg} src='https://i.imgur.com/QnAPvcN.png'></img></Link>
         <div className={styles.white}></div>
         </div>
     )
